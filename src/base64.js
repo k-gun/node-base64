5e77f7aef18b80800e3d28722fcb58b3bf04641a
Kerem Gunes <qeremy@gmail.com>
2015-11-14 04:14:01 +0200

    Add docs.

--- src/base64.js
+++ src/base64.js
@@ -6,11 +6,38 @@
  *    <http://opensource.org/licenses/MIT>
  */
 
+/**
+ * Base64 object.
+ * @public
+ *
+ * @type {Object}
+ */
 var Base64 = {
+    /**
+     * Encoding.
+     * @type {String}
+     */
     encoding: "utf-8",
+
+    /**
+     * Encoder.
+     * @public
+     *
+     * @param  {String} data
+     * @return {String}
+     */
     encode: function(data){
         return (new Buffer(data)).toString("base64");
     },
+
+    /**
+     * Decoder.
+     * @public
+     *
+     * @param  {String} data
+     * @param  {String} encoding
+     * @return {String}
+     */
     decode: function(data, encoding){
         return (new Buffer(data, "base64")).toString(encoding || this.encoding);
     }
